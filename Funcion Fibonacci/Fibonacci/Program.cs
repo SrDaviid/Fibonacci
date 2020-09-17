5e1a1d858b75d3ac0842b23c4bbe013f48387687
SrDaviid <70109934+SrDaviid@users.noreply.github.com>
2020-09-17 14:49:13 -0700

    Add files via upload

---
+++ Funcion Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Fibonacci
+{
+    class MainClass
+    {
+        const int Dato = 10000000;
+        static bool[] Condicion = new bool[Dato];
+
+        static void Main(string[] args)
+        {
+            var X = FuncionFibonacci();
+            while(true)
+            {
+                int Modificador = X();
+                if (Modificador == -1) break;
+
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine(Modificador);
+            }
+        }
+
+        static Func<int> FuncionFibonacci()
+        {
+            int a = 1;
+            return () =>
+            {
+                if (a >= Dato) return -1;
+                while (a < Dato && Condicion[++a]) ;
+                int c = 1;
+                while (a * c < Dato)
+                {
+                    Condicion[a * c] = true;
+                    c++;
+                }
+                return a;
+            };
+            
+        }
+
+    }
+}
